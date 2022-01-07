d912b7105469076979acbb7abd7e22035c473150
wangchunyan01 <wangchunyan1@jiandan100.com>
2022-01-07 18:05:51 +0800

    'react-router'

---
+++ react-router-demo/README.md
@@ -0,0 +1,11 @@
+1.react-router的基本使用
+
+2.BrowserRouter、MemoryRouter、HashRouter属于路由组件的父类，需要由这些组件包裹，才可以使用Route、Link、Switch组件
+
+3.Route渲染组件的三种方式
+1.children:func(在没有switch组件的情况下，不管路由是否匹配children都会渲染;)
+2.render:func
+3.component:组件(当使用内联函数的时候，应该使用children或者render的方式渲染组件；)
+
+渲染优先级关系：children > component >  render
+
---
+++ react-router-demo/src/router/index.js
@@ -0,0 +1,31 @@
+/**
+ * create by wangchunyan1 on 2022/1/7
+ */
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+import HomePage from "../pages/HomePage";
+import UserPage from "../pages/UserPage";
+import _404Page from "../pages/_404Page";
+export default function Routes(){
+
+    return(
+        <div>
+            <Router>
+                <Link to={'/'}>首页</Link>
+                <Link to={'/center'}>中心</Link>
+                <Switch>
+                    <Route path={'/'} exact component={HomePage}/>
+                    <Route path={'/center'} component={UserPage}/>
+                    <Route component={_404Page}/>
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+function render(){
+    return <h2>render方式</h2>
+}
+
+function children(){
+    return <h2>children方式</h2>
+}
